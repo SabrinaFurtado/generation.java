b7aae96d4ca52024da803a7b0637b92a7751f724
Sabrina Furtado <145471677+SabrinaFurtado@users.noreply.github.com>
2024-02-16 17:30:15 -0300

    Update calculoDiferenca.java

---
+++ calculoDiferenca.java
@@ -0,0 +1,15 @@
+package calcularDiferenca;
+
+public class calculoDiferenca {
+    public static void main(String[] args) {
+
+        float n1 = 5.0f;
+        float n2 = 6.0f;
+        float n3 = 7.0f;
+        float n4 = 8.0f;
+        
+        float diferenca = (n1 * n2) - (n3 * n4);
+        
+        System.out.println("Diferença: " + diferenca);
+    }
+}
